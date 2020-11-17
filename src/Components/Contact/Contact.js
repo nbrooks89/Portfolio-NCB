3717aa243c7ce8b9908c847fea45748c2c966212
Nicole Brooks <nbrooks2389@gmail.com>
2020-11-16 22:29:20 -0500

    css

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,5 +1,8 @@
 import React from "react";
 import "../../scss/Contact.scss";
+import IosCall from "react-ionicons/lib/IosCall";
+import IosPin from "react-ionicons/lib/IosPin";
+import IosMail from "react-ionicons/lib/IosMail";
 // import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 // import { faEnvelope } from "@fortawesome/free-regular-svg-icons";
 
@@ -17,8 +20,15 @@ function Contact() {
               {/* <FontAwesomeIcon icon={faEnvelope} size="lg" /> */}
               Get In Touch
             </div>
+
+
           </a>
         </div>
+            <div className="contacts-list">
+            <div className="contacts-item"><IosCall fontSize="30px" color="white" /> &nbsp;774-261-2814</div>
+            <div className="contacts-item"><IosPin fontSize="30px" color="white" />&nbsp;Boston,MA</div>
+            <div className="contacts-item-3"><IosMail fontSize="30px" color="white" />&nbsp;nbrooks2389@gmail.com</div>
+            </div>
         {/* <a href="https://www.linkedin.com/in/nicole-brooks89/">linkendin</a> */}
       </div>
     </>
